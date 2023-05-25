87d2d2304768883ddc863558b6fa73b41639ef57
Thibs <88190704+lethib@users.noreply.github.com>
2023-05-25 13:57:40 +0200

    [FAM-4] Typed Hooks Set Up (#14)

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,26 +1,26 @@
-import axios, { AxiosError, AxiosResponse, AxiosInstance } from 'axios'
+import axios, { AxiosError, AxiosResponse, AxiosInstance } from "axios";
 
 class Api {
-    baseURL: string;
-    client: AxiosInstance;
+  baseURL: string;
+  client: AxiosInstance;
 
-    constructor(baseURL: string) {
-        this.baseURL = baseURL;
-        this.client = axios.create({baseURL})
+  constructor(baseURL: string) {
+    this.baseURL = baseURL;
+    this.client = axios.create({ baseURL });
 
-        this.client.interceptors.response.use(
-            (response: AxiosResponse) => response,
-            (error: AxiosError) => {
-                const { data: errorData } = error.response!;
-                console.error(errorData)
-            }
-        )
-    }
+    this.client.interceptors.response.use(
+      (response: AxiosResponse) => response,
+      (error: AxiosError) => {
+        const { data: errorData } = error.response!;
+        console.error(errorData);
+      }
+    );
+  }
 
-    get = async <T>(path: string): Promise<T> => {
-        const result = await this.client.get<T>(path)
-        return result.data
-    }
+  get = async <T>(path: string): Promise<T> => {
+    const result = await this.client.get<T>(path);
+    return result.data;
+  };
 }
 
-export const familyApiInstance = new Api('http://localhost:8000')
+export const familyApiInstance = new Api("http://localhost:8000/api");
